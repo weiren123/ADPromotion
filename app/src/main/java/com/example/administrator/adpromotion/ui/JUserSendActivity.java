362fa27d472699e1a13c378b438aef18f5068114
15122042182@163.com <15122042182@163.com>
2018-04-19 22:26:56 +0800

    布局设计

---
+++ app/src/main/java/com/example/administrator/adpromotion/ui/JUserSendActivity.java
@@ -0,0 +1,15 @@
+package com.example.administrator.adpromotion.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.administrator.adpromotion.R;
+
+public class JUserSendActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_juser_send);
+    }
+}
---
+++ app/src/main/java/com/example/administrator/adpromotion/ui/SUserAnswerActivity.java
@@ -0,0 +1,15 @@
+package com.example.administrator.adpromotion.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.administrator.adpromotion.R;
+
+public class SUserAnswerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_suser_answer);
+    }
+}
